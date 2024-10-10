0162fd50fb5e40befb23e68a5ea5e22fcd2041d6
pasqualevitiello <pasqualevitiello@gmail.com>
2024-10-10 18:18:48 +0200

    up

--- app/input-37.tsx
+++ app/input-37.tsx
@@ -2,7 +2,7 @@
 
 import { DateInput, DateSegment, Label, TimeField } from "react-aria-components";
 
-export default function Input37() {
+export default function Input37() { 
   return (
     <TimeField className="space-y-2">
       <Label className="text-sm font-medium text-foreground">Time input</Label>
